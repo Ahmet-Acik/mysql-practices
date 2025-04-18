6344e73ae2227219cf09acfc4ff8e8549c1a4fa4
drahmetacik <a.acik@icloud.com>
2025-04-18 20:13:46 +0100

    refactor AdvancedSQLPractice to use a constant for the SQL query and improve error handling with logging

--- src/main/java/org/ahmet/practices/AdvancedSQLPractice.java
+++ src/main/java/org/ahmet/practices/AdvancedSQLPractice.java
@@ -5,14 +5,13 @@ import org.ahmet.utils.DatabaseUtil;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AdvancedSQLPractice {
-    public static void main(String[] args) {
-        try (Connection connection = DatabaseUtil.getConnection();
-             Statement statement = connection.createStatement()) {
-
-            // Example: Inner Join with Filtering
-            String query = """
+    private static final Logger LOGGER = Logger.getLogger(AdvancedSQLPractice.class.getName());
+    private static final String QUERY = """
                 SELECT e.name AS employee_name, d.name AS department_name, p.name AS project_name
                 FROM employees e
                 JOIN departments d ON e.dept_id = d.id
@@ -21,16 +20,26 @@ public class AdvancedSQLPractice {
                 WHERE d.name = 'IT';
             """;
 
-            ResultSet resultSet = statement.executeQuery(query);
+    public static void main(String[] args) {
+        try (Connection connection = DatabaseUtil.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(QUERY)) {
 
             while (resultSet.next()) {
-                System.out.println("Employee: " + resultSet.getString("employee_name") +
-                                   ", Department: " + resultSet.getString("department_name") +
-                                   ", Project: " + resultSet.getString("project_name"));
+                try {
+                    String employeeName = resultSet.getString("employee_name");
+                    String departmentName = resultSet.getString("department_name");
+                    String projectName = resultSet.getString("project_name");
+
+                    LOGGER.info(() -> String.format("Employee: %s, Department: %s, Project: %s",
+                            employeeName, departmentName, projectName));
+                } catch (SQLException e) {
+                    LOGGER.log(Level.SEVERE, "Error retrieving data from ResultSet: {0}", e.getMessage());
+                }
             }
 
         } catch (Exception e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "An error occurred while executing the query: {0}", e.getMessage());
         }
     }
 }
